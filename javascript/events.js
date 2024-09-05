e4575c595e2169035067329c5e29bb068bb5b907
samuel.flavin <sflavin2015@outlook.com>
2024-09-04 22:30:42 -0500

    did sum stuff

--- javascript/events.js
+++ javascript/events.js
@@ -1,5 +1,13 @@
 
 $(document).ready(function(){
+    $(document).click(function(event) {
+        if (document.getElementById("hamburger-button").contains(event.target)) {
+            manage_sidebar();
+        } else {
+            manage_sidebar("close");
+        }
+    });
+
     $(document).on('mouseover', '#hamburger-button', () => {
         document.body.style.cursor = 'pointer';
     });
@@ -9,16 +17,21 @@ $(document).ready(function(){
     });
 
     $(document).on('click', '#hamburger-button', () => {
-        manage_sidebar();
     })
 });
 
-function manage_sidebar() {
+function manage_sidebar(method="none") {
     let sidebar = document.getElementById('sidebar');
-   if (sidebar.style.visibility === "visible") {
-       sidebar.style.visibility = "hidden";
-   } else {
-       sidebar.style.visibility = "visible";
-   }
+    if (method === "none") {
+        if (sidebar.style.visibility === "visible") {
+            sidebar.style.visibility = "hidden";
+        } else {
+            sidebar.style.visibility = "visible";
+        }
+    }
+
+    if (method === "close") {
+        sidebar.style.visibility = "hidden";
+    }
 }
 
