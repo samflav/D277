9ce96202becaae9937874460fac445c391389274
samuel.flavin <sflavin2015@outlook.com>
2024-10-08 15:17:16 -0500

    still working on scripts

--- scripts/pa_git.sh
+++ scripts/pa_git.sh
@@ -5,16 +5,16 @@ rm -rf staging/
 cp -r D277/src/ staging/
 for FILE in staging/*.html; do
 	new=${new%.html}
-	sed '/<div id=\"nav-placeholder\">/r staging/assets/nav.html' $FILE > staging/$new
-	sed '/<div id=\"nav-placeholder\">/d' > staging/$new
-	rm $FILE
+	sed '/<div id=\"nav-placeholder\">/r staging/assets/nav.html' $FILE > staging/"$new"
+	sed '/<div id=\"nav-placeholder\">/d' > staging/"$new"
+	rm "$FILE"
 	echo "Fixing $new"
 done
 for FILE in src/cities/*.html; do
 	new=${new%.html}
-	sed '/<div id=\"nav-placeholder\">/r staging/assets/nav.html' $FILE > staging/$new
-	sed '/<div id=\"nav-placeholder\">/d' > staging/$new
-	rm $FILE
+	sed '/<div id=\"nav-placeholder\">/r staging/assets/nav.html' $FILE > staging/"$new"
+	sed '/<div id=\"nav-placeholder\">/d' > staging/"$new"
+	rm "$FILE"
 	echo "Fixing $new"
 done
 id=`docker ps -aqf "name=webserver"`
