6259cabc127e5b3146cfc471a7133c60fec6ceb9
samuel.flavin <sflavin2015@outlook.com>
2024-11-15 00:37:33 -0600

    fixed the links

--- src/javascript/events.js
+++ src/javascript/events.js
@@ -40,7 +40,7 @@ function toggle_sidebar(event) {
 function replace_link() {
     let link = document.getElementById("Specific-Link")
     if (link) {
-        link.style.display = "content";
+        link.style.display = "inline-block";
         document.getElementById("Generic-Link").replaceWith(link);
     }
     document.getElementById("nav-links").style.display = "inline-block";
